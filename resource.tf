e5f7147751eb608d16c4c9fee30c6816582f01b8
jk.karan375@gmail.com <jk.karan375@gmail.com>
2025-07-16 09:53:42 +0000

    Commit

--- resource.tf
+++ resource.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "storeterra" {
 }
  
 resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorage238"
+    name                     = "terrastorage2319"
     resource_group_name      = azurerm_resource_group.storeterra.name
     location                 = azurerm_resource_group.storeterra.location
     account_tier             = "Standard"
